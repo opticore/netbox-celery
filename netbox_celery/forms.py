21ec7f241fd78eebbd9b2750e7ebc221663dadbd
Kristian Carbonaro <carbonarok@gmail.com>
2023-03-29 13:48:37 +0100

    Removed unused import

--- netbox_celery/forms.py
+++ netbox_celery/forms.py
@@ -8,7 +8,6 @@ from django.utils.translation import gettext as _
 from users.models import User
 from utilities.forms import (
     APISelectMultiple,
-    BootstrapMixin,
     DateTimePicker,
     DynamicModelMultipleChoiceField,
     MultipleChoiceField,
