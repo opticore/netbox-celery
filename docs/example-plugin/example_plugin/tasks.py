fbac821622b412d3cf44e18f86502161912c8caf
Kristian Carbonaro <carbonarok@gmail.com>
2023-03-30 11:49:20 +0100

    Fixed pylint error

--- docs/example-plugin/example_plugin/tasks.py
+++ docs/example-plugin/example_plugin/tasks.py
@@ -3,6 +3,6 @@ from netbox_celery.tasks import netbox_celery_task
 
 
 @netbox_celery_task(name="example_plugin:hello_world")
-def hello_world(self, task_id):
+def hello_world(self, task_id):  # pylint: disable=unused-argument
     """Example hello world task."""
     self.log("Hello World!")
