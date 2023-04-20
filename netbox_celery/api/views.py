01dc9e847e19fb433428c41f4528b16904765aac
apishark <james.curtis@opticoreit.com>
2023-04-20 09:51:19 +0100

    pylint

--- netbox_celery/api/views.py
+++ netbox_celery/api/views.py
@@ -2,7 +2,10 @@
 from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from netbox_celery.api.serializers import CeleryResultSerializer, ResultLogSerializer
+from netbox_celery.api.serializers import (
+    CeleryResultSerializer,
+    ResultLogSerializer
+)
 from netbox_celery.filtersets import CeleryResultFilterSet
 from netbox_celery.models import CeleryResult, CeleryLogEntry
 
@@ -22,14 +25,19 @@ class CeleryResultView(NetBoxModelViewSet):
 
 
 class ResultLogViewSet(ReadOnlyModelViewSet):
+    """ResultLog ViewSet."""
     queryset = CeleryLogEntry.objects.all()
     serializer_class = ResultLogSerializer
 
-    def retrieve(self, request, pk=None):
+    def retrieve(self, request, *args, **kwargs):
+        job_result = kwargs['pk']
         latest = self.request.query_params.get("latest")
         if latest:
-            log = CeleryLogEntry.objects.filter(job_result=pk, created__gt=latest)
+            log = CeleryLogEntry.objects.filter(
+                job_result=job_result,
+                created__gt=latest
+                )
         else:
-            log = CeleryLogEntry.objects.filter(job_result=pk)
+            log = CeleryLogEntry.objects.filter(job_result=job_result)
         serializer = self.get_serializer(log, many=True)
         return Response(serializer.data)
